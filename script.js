3da35903a53798e494efa6c080f9b7da93b4ca8a
Tymku <103423416+Tymku@users.noreply.github.com>
2024-10-15 12:45:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ let l = document.getElementById("licznik");
 let w = document.getElementById("wynik");
 let c = document.getElementById("sprawdz")
 
+/*function check_num(){
+    /*var reg = /[0-9]/g;
+    var ntab = licz.match(reg);
+    if(ntab!=null){
+        check=true;
+        console.log(check);
+    }
+    else{
+        check=false;
+        console.log(check);
+    }*/
+
+    /*if(licz.toString().indexOf(toString(7)) > -1 /* true if jest 7 ){
+        check = true;
+    }
+    else{
+        check = false;
+    }
+
+    check = (licz.toString().indexOf(toString(7)) >= -1);
+}*/
+
 function licznik(){
     licz++;
     l.innerHTML = licz;
@@ -17,7 +39,8 @@ function wynik(){
 }
 
 function graj(){
-    if(licz%7==0 || licz.toString().indexOf("7") >= -1){
+    //check_num();
+    if(licz%7==0 || licz.toString().indexOf("7") > -1){
         wyn++;
     }else{
         wyn--;
